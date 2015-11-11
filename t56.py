3d24b704ea90a0816fc946f015ee978b04278253
zhouwangyiteng <zhouwang122@qq.com>
2015-11-11 23:27:06 +0800

    add t57

--- t56.py
+++ t56.py
@@ -6,10 +6,10 @@ canvas = Canvas(width=400, height=300, bg='green')
 canvas.pack(expand=YES, fill=BOTH)
 
 k = 1
-j = 1
-for i in range(0, 26):
+j = 5
+for i in range(0, 10):
     canvas.create_oval(210-k, 150-k, 210+k, 150+k, width=1)
     k += j
-    j += 0.3
+    j += 1.5
 
 mainloop()
