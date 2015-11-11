e80a4d58a75dd3fc8cef461c22a95aeb24e5dd8d
zhouwangyiteng <zhouwang122@qq.com>
2015-11-11 23:17:37 +0800

    add t56

---
+++ t56.py
@@ -0,0 +1,15 @@
+# _*_ coding: UTF-8 _*_
+
+from Tkinter import *
+
+canvas = Canvas(width=400, height=300, bg='green')
+canvas.pack(expand=YES, fill=BOTH)
+
+k = 1
+j = 1
+for i in range(0, 26):
+    canvas.create_oval(210-k, 150-k, 210+k, 150+k, width=1)
+    k += j
+    j += 0.3
+
+mainloop()
