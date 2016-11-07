95d4bffdf6f84d7134e05c172e23f82a3f5d5c01
zhouwangyiteng <zhouwang122@qq.com>
2016-11-07 16:39:20 +0800

    新文件:       autologinjwc/autoSc.py

---
+++ autologinjwc/autoSc.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+from selenium import webdriver
+import Image
+
+
+def getCode():
+    browser = webdriver.Firefox()
+    url = "http://xk.autoisp.shu.edu.cn/"
+    browser.get(url)
+    browser.save_screenshot("codingpy.png")
+    element = browser.find_element_by_css_selector('#Img1')
+    left = int(element.location['x'])
+    top = int(element.location['y'])
+    right = int(element.location['x'] + element.size['width'])
+    bottom = int(element.location['y'] + element.size['height'])
+
+    im = Image.open("codingpy.png")
+    im = im.crop((left, top, right, bottom))
+
+    browser.close()
+    browser.quit()
+
+    return im
