bb7232cb035c0ca3d97696731568746d4316e2bc
zhouwangyiteng <zhouwang122@qq.com>
2016-11-08 22:03:30 +0800

    finished col-split

--- autologinjwc/learnp.py
+++ autologinjwc/learnp.py
@@ -50,22 +50,73 @@ def clearPic(picNum):
     img = Image.open(file_dir + stype + num + filePost)
     img = img.convert("L")
     WHITE, BLACK = 255, 0
-    sumN = img.size[0]*img.size[1]
-    sumP =0
+    sumN = img.size[0] * img.size[1]
+    sumP = 0
     for x in xrange(img.size[0]):
-            for y in xrange(img.size[1]):
-                sumP += img.getpixel((x, y))
-    ttt = sumP/sumN
-    ttt-=29
+        for y in xrange(img.size[1]):
+            sumP += img.getpixel((x, y))
+    ttt = sumP / sumN
+    ttt -= 29
     img = img.point(lambda x: 1 if x > ttt else 0, '1')
     clearNoise(img, 3, 5)
-    img.save(file_dir + dtype + num + filePost)
+    return img
 
 
-def main():
-    for i in range(500):
-        clearPic(i)
-        print i
+def colSplit(img):
+    rowP = img.size[0] * [1]
+    for i in xrange(1, img.size[1] - 1):
+        for j in xrange(img.size[0]):
+            rowP[j] = rowP[j] & img.getpixel((j, i))
+    splitF = []
+    rowP = [1, 1] + rowP + [1, 1, 1]
+    l = 0
+    r = 0
+    for i in xrange(img.size[0]):
+        if rowP[i + 2] == 1:
+            if l < r:
+                splitF += [l, r]
+            l = i + 1
+            r = l
+        else:
+            r = i + 1
+
+    return splitF
 
 
-main()
+def rowSplit(img):
+    colP = img.size[1] * [1]
+    for i in xrange(1, img.size[0] - 1):
+        for j in xrange(img.size[1]):
+            colP[j] = colP[j] & img.getpixel((i, j))
+    splitF = []
+    for i in xrange(img.size[1]):
+        if colP[i + 2] == 0:
+                splitF.append(i)
+                break
+    for i in xrange(0,img.size[1],-1):
+        if colP[i + 2] == 0:
+                splitF.append(i+1)
+                break
+    return splitF
+
+
+def splitPic(num):
+    im = Image.open('codeset/cleareddata/' + str(num) + '.png')
+    splitF = colSplit(im)
+    lens = len(splitF) / 2
+    cnt = 0
+    for i in range(lens):
+        if (splitF[2 * i + 1] - splitF[2 * i]) <= 1 or (splitF[2 * i + 1] - splitF[2 * i]) > 16:
+            continue
+        splitIm = im.crop((splitF[2 * i], 0, splitF[2 * i + 1], 21))
+        splitIm.save('codeset/splitdata/' + str(num) + '_' + str(cnt) + '.png')
+        cnt += 1
+    # for i in range(cnt):
+    #     im = Image.open('codeset/splitdata/' +
+    #                     str(num) + '_' + str(i) + '.png')
+    #     splitF = rowSplit(im)
+    #     splitIm = im.crop((0, splitF[0], im.size[0] - 1, splitF[-1]))
+    #     splitIm.save('codeset/splitdata/' + str(num) + '_' + str(i) + '.png')
+
+for i in range(20):
+    splitPic(i)
